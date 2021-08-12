e2a5dc980a41d0b5fe76f618165d26e2a1d0bea3
Shubham gaur <gaur7699@gmail.com>
2021-08-12 17:53:54 +0530

    testing left i guess

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,14 +1,19 @@
 import React, { useState,useContext,useEffect } from 'react';
 import { SelectProfileContainer } from './profiles';
+import Fuse from 'fuse.js';
+import {FooterContainer} from './footer';
 import {FirebaseContext} from '../context/firebase';
-import {Loading,Header} from '../components';
+import {Card,Loading,Header,Player} from '../components';
 import * as ROUTES from '../constants/routes';
 import logo from '../logo.svg';
 
 export function BrowseContainer({slides}){
     const [searchTerm, setSearchTerm] = useState('');
+    const [category,setCategory] = useState('series');
     const [profile,setProfile] = useState({});
-    const [loading,setLoading] = useState(true)
+    const [loading,setLoading] = useState(true);
+    const [slideRows,setSlideRows] = useState([])
+
     const {firebase} = useContext(FirebaseContext);
     const user = firebase.auth().currentUser ||{}
 
@@ -17,6 +22,22 @@ export function BrowseContainer({slides}){
             setLoading(false);
         },3000);
     },[profile.displayName]);
+
+    useEffect(()=>{
+        setSlideRows(slides[category]);
+    },[slides,category])
+
+    useEffect(()=>{
+        const fuse = new Fuse(slideRows,{ keys:['data.description','data.genre','data.title']});
+        const results = fuse.search(searchTerm).map(({item}) => item);
+        if (slideRows.length>0 && searchTerm.length>3 && results.length>0) {
+            setSlideRows(results);
+        }
+        else {
+            setSlideRows(slides[category]);
+        }
+    },[searchTerm])
+
     return profile.displayName ? (
         <>
         {loading ? (<Loading src={user.photoURL} />
@@ -28,8 +49,19 @@ export function BrowseContainer({slides}){
 
                 <Header.Group>
                   <Header.Logo to={ROUTES.HOME} src={logo} alt='Netflix' />
-                  <Header.TextLink>Series</Header.TextLink>
-                  <Header.TextLink>Films</Header.TextLink>
+                  <Header.TextLink 
+                    active={category ==='series'? 'true':'false'}
+                    onClick={()=>setCategory('series')}
+                  >
+                      Series
+                  </Header.TextLink>
+
+                  <Header.TextLink
+                    active={category ==='films'? 'true':'false'}
+                    onClick={()=>setCategory('films')}
+                  >
+                      Films
+                  </Header.TextLink>
                 </Header.Group>
 
                 <Header.Group>
@@ -61,6 +93,34 @@ export function BrowseContainer({slides}){
                 <Header.PlayButton>Play</Header.PlayButton>
             </Header.Feature>
         </Header>
+
+
+        <Card.Group>
+            {slideRows.map((slideItem) =>(
+                <Card key ={`${category}-${slideItem.title.toLowerCase()}`}>
+                    <Card.Title>{slideItem.title}</Card.Title>
+                    <Card.Entities>
+                        {slideItem.data.map((item) =>(
+                            <Card.Item key={item.docId} item={item}>
+                                <Card.Image src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`} />
+                                <Card.Meta>
+                                    <Card.SubTitle>{item.title}</Card.SubTitle>
+                                    <Card.Text>{item.description}</Card.Text>
+                                </Card.Meta>
+
+                            </Card.Item>
+                        ))}
+                    </Card.Entities>
+                    <Card.Feature category={category}>
+                        <Player>
+                          <Player.Button />
+                          <Player.Video src="/videos/bunny.mp4" />
+                        </Player>
+                    </Card.Feature> 
+                </Card>
+            ))}
+        </Card.Group>
+        <FooterContainer />
         </>
     ): (<SelectProfileContainer user={user} setProfile={setProfile} />);
 }
